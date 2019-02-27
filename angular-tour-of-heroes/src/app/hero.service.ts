54b88d8d86ed388d3f790e9fde2308c8e868ce90
kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2019-02-27 10:38:06 +0530

    hero-service created

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './heroes/mock-heroes';
+import {Hero} from './hero';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
